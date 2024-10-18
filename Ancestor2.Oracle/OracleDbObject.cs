b2f58db9c1d5483a8f9b1954ec99da1152d0ecbd
Nagi Lin <nagilin@cgmh.org.tw>
2024-10-18 19:43:12 +0800

    init project

---
+++ Ancestor2.Oracle/OracleDbObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ancestor.Oralce
+{
+    /// <summary>
+    /// Oracle db object
+    /// </summary>
+    public class OracleDbObject : IDbObject
+    {
+        
+    }
+}
